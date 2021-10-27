e89fa75d638145a7ab5b8500d8b9f445b42e89ec
Dmitry Katasonov <dmitry.katasonov@deleteagency.com>
2021-10-27 17:38:54 +0500

    Add db logging to debug output

--- BookService/DbContext/BookContext.cs
+++ BookService/DbContext/BookContext.cs
@@ -7,6 +7,7 @@ namespace BookService.DbContext
     {
         public BookContext() : base("name=BookService")
         {
+            Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
         }
         public DbSet<Author> Authors {get;set;}
         public DbSet<Book> Books {get;set;}
